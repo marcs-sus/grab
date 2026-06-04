aeca19598e5f3baf7efe4119e13d3ff3a25e6288
marcs-sus <marcosviniciuscunha@proton.me>
2026-06-04 15:01:39 -0300

    Improve Result/Error handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use anyhow::{Context, Result};
 use clap::Parser;
 use std::{
     fs::File,
@@ -10,14 +11,17 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<()> {
     let args = Cli::parse();
 
-    let f = File::open(&args.path)?;
+    let f = File::open(&args.path)
+        .with_context(|| format!("Could not open file `{}`", args.path.display()))?;
     let mut reader = BufReader::new(f);
 
     let mut content = String::new();
-    let _len = reader.read_to_string(&mut content)?;
+    let _len = reader
+        .read_to_string(&mut content)
+        .with_context(|| format!("Could not read file `{}`", args.path.display()))?;
 
     for line in content.lines() {
         if line.contains(&args.pattern) {
