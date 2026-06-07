f826dc866d72c6c391afc10090813d909c4f1adc
marcs-sus <marcosviniciuscunha@proton.me>
2026-06-07 12:52:32 -0300

    Optimize reading and writing performance

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use anyhow::{Context, Result};
 use clap::Parser;
 use std::{
     fs::File,
-    io::{BufReader, Read},
+    io::{self, BufRead, BufReader, BufWriter, Read, Write},
 };
 
 #[derive(Parser)]
@@ -12,20 +12,24 @@ struct Cli {
 }
 
 fn main() -> Result<()> {
+    // Fetch arguments
     let args = Cli::parse();
 
+    // Open file
     let f = File::open(&args.path)
         .with_context(|| format!("Could not open file `{}`", args.path.display()))?;
-    let mut reader = BufReader::new(f);
 
-    let mut content = String::new();
-    let _len = reader
-        .read_to_string(&mut content)
-        .with_context(|| format!("Could not read file `{}`", args.path.display()))?;
+    // Create reading and writing buffers
+    let reader = BufReader::new(f);
+    let mut writer = BufWriter::new(io::stdout().lock());
 
-    for line in content.lines() {
+    // Write found lines
+    for line in reader.lines() {
+        let line =
+            line.with_context(|| format!("Could not read line from `{}`", args.path.display()))?;
+
         if line.contains(&args.pattern) {
-            println!("{}", line)
+            writeln!(writer, "{line}")?;
         }
     }
 
